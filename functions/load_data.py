dab80af56a2e66ccb86bc09638255b15c768ed66
tcf0005 <tcf0005@auburn.edu>
2023-08-02 12:08:52 -0500

    commit

---
+++ functions/load_data.py
@@ -0,0 +1,111 @@
+import numpy as np
+import scipy.sparse as sp
+import torch
+
+path_in = "./data/"
+
+#OG
+dataset_in = "cora"
+
+#new_texas
+# dataset_in = "texas"
+
+#new_cornell
+# dataset_in = "cornell-new"
+
+# new_wisconsin 
+# dataset_in = "wisconsin-new"
+
+# new_washington
+# dataset_in = "washington-new"
+
+def encode_onehot(labels):
+    # The classes must be sorted before encoding to enable static class encoding.
+    # In other words, make sure the first class always maps to index 0.
+    classes = sorted(list(set(labels)))
+    classes_dict = {c: np.identity(len(classes))[i, :] for i, c in enumerate(classes)}
+    labels_onehot = np.array(list(map(classes_dict.get, labels)), dtype=np.int32)
+    return labels_onehot
+
+
+
+
+def load_data(path=path_in, dataset=dataset_in):
+    """Load citation network dataset (cora only for now)"""
+    print('Loading {} dataset...'.format(dataset))
+
+    idx_features_labels = np.genfromtxt("{}{}.content".format(path, dataset), dtype=np.dtype(str))
+    features = sp.csr_matrix(idx_features_labels[:, 1:-1], dtype=np.float32)
+    labels = encode_onehot(idx_features_labels[:, -1])
+
+    # build graph
+    # print(idx_features_labels[:, 0])
+    idx = np.array(idx_features_labels[:, 0], dtype=np.int32)
+    idx_map = {j: i for i, j in enumerate(idx)}
+    edges_unordered = np.genfromtxt("{}{}.cites".format(path, dataset), dtype=np.int32)
+    edges = np.array(list(map(idx_map.get, edges_unordered.flatten())), dtype=np.int32).reshape(edges_unordered.shape)
+    adj = sp.coo_matrix((np.ones(edges.shape[0]), (edges[:, 0], edges[:, 1])), shape=(labels.shape[0], labels.shape[0]), dtype=np.float32)
+
+    # build symmetric adjacency matrix
+    adj = adj + adj.T.multiply(adj.T > adj) - adj.multiply(adj.T > adj)
+
+    features = normalize_features(features)
+    adj = normalize_adj(adj + sp.eye(adj.shape[0]))
+
+    #Cora 
+    # idx_train = range(140)
+    # idx_val = range(200, 500)
+    # idx_test = range(500, 1500)
+
+
+    #New Texas/Wisconsin
+    # idx_train = range(60)
+    # idx_val = range(61, 121)
+    # idx_test = range(122, 187)
+
+    #New Cornell 
+    # idx_train = range(90)
+    # idx_val = range(91, 139)
+    # idx_test = range(140, 195)
+
+    #New Washington
+    idx_train = range(128)
+    idx_val = range(129, 179)
+    idx_test = range(180, 230)
+
+
+    adj = torch.FloatTensor(np.array(adj.todense()))
+    features = torch.FloatTensor(np.array(features.todense()))
+    labels = torch.LongTensor(np.where(labels)[1])
+
+    idx_train = torch.LongTensor(idx_train)
+    idx_val = torch.LongTensor(idx_val)
+    idx_test = torch.LongTensor(idx_test)
+
+    return adj, features, labels, idx_train, idx_val, idx_test
+
+
+def normalize_adj(mx):
+    """Row-normalize sparse matrix"""
+    rowsum = np.array(mx.sum(1))
+    r_inv_sqrt = np.power(rowsum, -0.5).flatten()
+    r_inv_sqrt[np.isinf(r_inv_sqrt)] = 0.
+    r_mat_inv_sqrt = sp.diags(r_inv_sqrt)
+    return mx.dot(r_mat_inv_sqrt).transpose().dot(r_mat_inv_sqrt)
+
+
+def normalize_features(mx):
+    """Row-normalize sparse matrix"""
+    rowsum = np.array(mx.sum(1))
+    r_inv = np.power(rowsum, -1).flatten()
+    r_inv[np.isinf(r_inv)] = 0.
+    r_mat_inv = sp.diags(r_inv)
+    mx = r_mat_inv.dot(mx)
+    return mx
+
+
+def accuracy(output, labels):
+    preds = output.max(1)[1].type_as(labels)
+    correct = preds.eq(labels).double()
+    correct = correct.sum()
+    return correct / len(labels)
---
+++ train.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python3
+from __future__ import division
+from __future__ import print_function
+
+import os
+import glob
+import time
+import random
+import argparse
+import numpy as np
+import torch
+import torch.optim as optim
+from torch.autograd import Variable
+from model.model import GAT
+from functions.load_data import load_data, accuracy
+import torch.nn.functional as F
+
+import csv
+import yaml
+
+
+
+#Read in Values from YAML File 
+with open('./params/params.yaml') as params:
+    param_dict = yaml.full_load(params)
+
+#Store Values in Variables for use later
+csv_file = param_dict["csv_file"]
+disable_cuda = param_dict["disable_cuda"]
+fastmode = param_dict["fastmode"]
+random_seed = param_dict["random_seed"]
+max_epochs = param_dict["max_epochs"]
+learning_rate = param_dict["learning_rate"]
+weight_decay = param_dict["weight_decay"]
+hidden = param_dict["hidden"]
+nb_heads = param_dict["nb_heads"]
+dropout = param_dict["dropout"]
+alpha = param_dict["alpha"]
+patience = param_dict["patience"]
+momentum = param_dict["momentum"]
+optimizer_type = param_dict["optimizer_type"]
+
+#Open CSV Reader
+f = open(csv_file, 'w')
+fieldnames = ['Epoch', 'loss_train', 'acc_val', 'loss_val', 'acc_val']
+writer = csv.writer(f)
+writer.writerow(fieldnames) 
+
+#Setting up Cuda
+cuda = not disable_cuda and torch.cuda.is_available()
+random.seed(random_seed)
+np.random.seed(random_seed)
+torch.manual_seed(random_seed)
+if cuda:
+    torch.cuda.manual_seed(random_seed)
+
+#Load Data
+adj, features, labels, idx_train, idx_val, idx_test = load_data()
+
+#Create GAT model
+model = GAT(nfeat=features.shape[1], nhid=hidden, nclass=int(labels.max()) + 1, dropout=dropout, nheads=nb_heads, alpha=alpha)
+
+#Define Optimizer 
+if optimizer_type == "ADAM":
+    print("Using ADAM Optimizer")
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay) #lr = 0.05
+elif optimizer_type == "Adadelta":
+    print("Using Adadelta Optimizer")
+    optimizer = optim.Adadelta(model.parameters(), lr=learning_rate)
+elif optimizer_type == "SGD":
+    print("Using SGD Optimizer")
+    optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum) 
+else:
+    print("Optimizer Type Not Recognized")
+
+
+#Load Parameters onto CUDA 
+if cuda:
+    model.cuda()
+    features = features.cuda()
+    adj = adj.cuda()
+    labels = labels.cuda()
+    idx_train = idx_train.cuda()
+    idx_val = idx_val.cuda()
+    idx_test = idx_test.cuda()
+
+features, adj, labels = Variable(features), Variable(adj), Variable(labels)
+
+#Define Loss Function Here
+def compute_loss(output, labels):
+    # original (negative-logliklihood loss):
+    loss_value = F.nll_loss(output, labels) #original (negative-logliklihood loss)
+
+    #CrossEntropyLoss
+    # cross_entropy_loss = nn.CrossEntropyLoss()
+    # loss_value = cross_entropy_loss(output, labels)
+    return loss_value
+
+#Define Training procedure
+def train(epoch):
+    t = time.time()
+    model.train()
+    optimizer.zero_grad()
+    output = model(features, adj)
+    loss_train = compute_loss(output[idx_train], labels[idx_train])
+    acc_train = accuracy(output[idx_train], labels[idx_train])
+    loss_train.backward()
+    optimizer.step()
+
+    if not fastmode:
+        # Evaluate validation set performance separately,
+        # deactivates dropout during validation run.
+        model.eval()
+        output = model(features, adj)
+
+    #loss_val = F.nll_loss(output[idx_val], labels[idx_val])
+    loss_val = compute_loss(output[idx_val], labels[idx_val])
+    acc_val = accuracy(output[idx_val], labels[idx_val])
+    print('Epoch: {:04d}'.format(epoch+1),
+          'loss_train: {:.4f}'.format(loss_train.data.item()),
+          'acc_train: {:.4f}'.format(acc_train.data.item()),
+          'loss_val: {:.4f}'.format(loss_val.data.item()),
+          'acc_val: {:.4f}'.format(acc_val.data.item()),
+          'time: {:.4f}s'.format(time.time() - t))
+
+    row = [epoch+1,loss_train.data.item(), acc_train.data.item(), loss_val.data.item(), acc_val.data.item()]
+    writer.writerow(row)
+
+    return loss_val.data.item()
+
+#Define Testing Procedure
+def compute_test():
+    model.eval()
+    output = model(features, adj)
+    #loss_test = F.nll_loss(output[idx_test], labels[idx_test])
+    loss_test = compute_loss(output[idx_train], labels[idx_train])
+    acc_test = accuracy(output[idx_test], labels[idx_test])
+    print("Test set results:",
+          "loss= {:.4f}".format(loss_test.data.item()),
+          "accuracy= {:.4f}".format(acc_test.data.item()))
+
+# Train model
+t_total = time.time()
+loss_values = []
+bad_counter = 0
+best = max_epochs + 1
+best_epoch = 0
+for epoch in range(max_epochs):
+    loss_values.append(train(epoch))
+    if loss_values[-1] < best:
+        best = loss_values[-1]
+        best_epoch = epoch
+        bad_counter = 0
+    else:
+        bad_counter += 1
+
+    if bad_counter == patience:
+        break
+
+    
+print("Optimization Finished!")
+print("Total time elapsed: {:.4f}s".format(time.time() - t_total))
+
+# Testing
+compute_test()
+
+f.close()
